5bdefecb6b2bafd617c9fd255c9ac553cf76093e
Austin Pocus <ajpocus@gmail.com>
2020-12-18 16:43:13 -0500

    test pipeline up and running w/ parcel + jest

--- __tests__/useListenerState.test.js
+++ __tests__/useListenerState.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-test("it sets state like normal useState");
-test("it listens for state changes on the entire object");
-test("it stops listening for changes");
-test("it listens for changes once");
-// TODO test that it listens for changes on a particular object.path.like.this
+test.todo("it sets state like normal useState");
+test.todo("it listens for state changes on the entire object");
+test.todo("it stops listening for changes");
+test.todo("it listens for changes once");
+test.todo("it listens for changes on a particular object.path.like.this");
