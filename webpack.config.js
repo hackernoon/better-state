eac06d5a30ff9419c2ac31cef9dd29ccb6e8ff60
Austin Pocus <ajpocus@gmail.com>
2020-12-18 16:33:50 -0500

    Save state -- trying to test, need bundler

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,25 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'index.js'
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js"
   },
   module: {
     rules: [
       {
         test: /\.m?js$/,
         exclude: [
-          /node_modules/
+          /node_modules/,
+          /__tests__/
         ],
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
             presets: [
-              ['@babel/preset-env', { targets: "defaults, not ie 11, maintained node versions" }]
+              ["@babel/preset-env", { targets: "defaults, not ie 11, maintained node versions" }],
+              "@babel/preset-react"
             ]
           }
         }
