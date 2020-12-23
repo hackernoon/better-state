e3edacaac89b6fd37c948e4211d942722198a403
Austin Pocus <ajpocus@gmail.com>
2020-12-22 19:30:22 -0500

    rm logs, try/catch on updateState

--- src/useUpdateState.js
+++ src/useUpdateState.js
@@ -7,58 +7,49 @@ export const useUpdateState = (initialState) => {
   }
   const [state, setState] = useState(initialState);
 
-  useEffect(() => console.log(state));
-
   const updateState = (pathname, newState) => {
-    try {
-      if (pathname && newState && isString(pathname)) {
-        // regular pathname / value update
-        
-        let stateCopy = cloneDeep(state);
-        set(stateCopy, pathname, newState);
-        setState(stateCopy);
-        return stateCopy;
-      } else if (pathname && !newState) {
-        // single param mode
-        newState = pathname;
-        if (isArray(state) && isArray(newState)) {
-          const newStateConcat = state.concat(newState)
-          setState(newStateConcat);
-          return newStateConcat;
-        } else if (isArray(state) && !isArray(newState)) {
-          const newStateConcat = state.concat([newState]);
-          setState(newStateConcat);
-          return newStateConcat;
-        } else if (isArray(newState)) {
-          setState(newState);
-          return newState;
-        } else if (isObject(newState)) {
-          // upsert object
-          const newStateSpread = { ...state, ...newState };
-          setState(newStateSpread);
-          return newStateSpread;
-        } else if (!isObject(newState) && !isNil(newState) && !isFunction(newState)) {
-          // new state is a primitive value, just set the state to that value
-          setState(newState);
-          return newState;
-        } else if (isFunction(newState)) {
-          // set the value of the function -- by default, passing a function will use useState's function handler
-          setState(() => newState);
-          return newState;
-        } else if (isNil(newState)) {
-          // do nothing, the dev may have not meant to pass a nil value
-          return state;
-        } else {
-          throw new Error("InvalidParameterError: updateState requires either a pathstring/value pair, or a single value to upsert");
-        }
+    if (pathname && newState && isString(pathname)) {
+      // regular pathname / value update
+      
+      let stateCopy = cloneDeep(state);
+      set(stateCopy, pathname, newState);
+      setState(stateCopy);
+      return stateCopy;
+    } else if (pathname && !newState) {
+      // single param mode
+      newState = pathname;
+      if (isArray(state) && isArray(newState)) {
+        const newStateConcat = state.concat(newState)
+        setState(newStateConcat);
+        return newStateConcat;
+      } else if (isArray(state) && !isArray(newState)) {
+        const newStateConcat = state.concat([newState]);
+        setState(newStateConcat);
+        return newStateConcat;
+      } else if (isArray(newState)) {
+        setState(newState);
+        return newState;
+      } else if (isObject(newState)) {
+        // upsert object
+        const newStateSpread = { ...state, ...newState };
+        setState(newStateSpread);
+        return newStateSpread;
+      } else if (!isObject(newState) && !isNil(newState) && !isFunction(newState)) {
+        // new state is a primitive value, just set the state to that value
+        setState(newState);
+        return newState;
+      } else if (isFunction(newState)) {
+        // set the value of the function -- by default, passing a function will use useState's function handler
+        setState(() => newState);
+        return newState;
+      } else if (isNil(newState)) {
+        // do nothing, the dev may have not meant to pass a nil value
+        return state;
       } else {
         throw new Error("InvalidParameterError: updateState requires either a pathstring/value pair, or a single value to upsert");
       }
-    } catch (err) {
-      console.error(err);
-      console.log("state: ", state);
-      console.log("pathname: ", pathname);
-      console.log("newState: ", newState);
+    } else {
+      throw new Error("InvalidParameterError: updateState requires either a pathstring/value pair, or a single value to upsert");
     }
   };
 
