79d597874d42626a78178bb73a8a4df07e42f336
Austin Pocus <ajpocus@gmail.com>
2021-01-05 14:12:49 -0500

    missed a spot

--- __tests__/useEmitterState.test.js
+++ __tests__/useEmitterState.test.js
@@ -1,58 +1,22 @@
 import React, { useState, useCallback, useMemo, useEffect } from "react";
-import { render, fireEvent, waitFor, act } from "@testing-library/react";
+import { render, fireEvent, waitFor } from "@testing-library/react";
+import { renderHook, act } from '@testing-library/react-hooks'
 import sinon from "sinon";
 import "@testing-library/jest-dom/extend-expect";
 
 import useEmitterState from "../src/useEmitterState";
 
-const EmitterState = ({ callback }) => {
-  const [counter, updateCounter, emitter] = useEmitterState(0);
-
-  useEffect(() => {
-    emitter.on("*", callback);
-
-    return () => {
-      emitter.off("*", callback);
-    };
-  }, []);
-
-  const plusCounter = useCallback(() => {
-    updateCounter(counter + 1);
-  }, [counter]);
-
-  const removeTestEmitter = () => {
-    emitter.off(".", callback);
-  };
-
-  const addOnceEmitter = () => {
-    emitter.once("*", callback);
-  };
-
-  return (
-    <div className="counter-container">
-      <div className="state">
-        <div className="counter-state" data-testid="counter-state">{counter || "0"}</div>
-        <div className="emitter-count" data-testid="emitter-count">{emitter.all.size}</div>
-      </div>
-
-      <div className="counter-controls">
-        <button className="plus btn" data-testid="plus-button" onClick={plusCounter}>+</button>
-        <button className="once-listener btn" data-testid="once-listener" onClick={addOnceEmitter}>Listen once</button>
-        <button data-testid="remove-listener" onClick={removeTestEmitter}>Remove listener</button>
-      </div>
-    </div>
-  );
-};
-
 test("it sets the default state", async () => {
-  const { getByTestId } = render(<EmitterState />);
-  fireEvent.click(getByTestId("plus-button"));
-  await waitFor(() => expect(getByTestId("counter-state")).toHaveTextContent("1"));
+  const { result } = renderHook(() => useEmitterState(0));
+  act(() => {
+    result.current.updateState(result.current.state + 1);
+  });
+  expect(result.current.state).toBe(1);
 });
 
 test.only("it listens for state changes on the entire object", async () => {
   const callback = sinon.fake();
-  const { getByTestId } = render(<EmitterState callback={callback} />);
+  const { result } = renderHook(() => useEmitterState(0));
   
   await waitFor(() => expect(getByTestId("counter-state")).toHaveTextContent("0"));
   act(() => {
@@ -63,7 +27,7 @@ test.only("it listens for state changes on the entire object", async () => {
 });
 
 test("it stops listening for changes", async () => {
-  const { getByTestId } = render(<EmitterState />);
+  const { result } = renderHook(() => useEmitterState(0));
 
   fireEvent.click(getByTestId("listener-button"));
   await waitFor(() => expect(getByTestId("listener-count")).toHaveTextContent("1"));
@@ -79,7 +43,7 @@ test("it stops listening for changes", async () => {
 });
 
 test("it listens for changes once", async () => {
-  const { getByTestId } = render(<EmitterState />);
+  const { result } = renderHook(() => useEmitterState(0));
 
   fireEvent.click(getByTestId("once-listener"));
   await waitFor(() => expect(getByTestId("listener-count")).toHaveTextContent("1"));
